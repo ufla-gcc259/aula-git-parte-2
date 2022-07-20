0959be2a7425d8192f1ab76da6836dad40c51e9e
Paulo <pauloafpjunior@hotmail.com>
2022-07-19 21:49:59 -0300

    Comentar funções do código

--- tempconv/tempconv.go
+++ tempconv/tempconv.go
@@ -1,3 +1,4 @@
+// tempconv realiza conversões de Celsius e Fahrenheit
 package tempconv
 
 import "fmt"
@@ -14,5 +15,8 @@ const (
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
 
+// CToF converte uma temperatura em Celsius para Fahrenheit
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// FToC converte uma temperatura em Fahrenheit para Celsius
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
