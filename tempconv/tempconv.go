e0c4615c0cb4c94ff5aa7a8c1e2bb4e978eb0963
Arthur Maia <arthur.maia@sydle.com>
2022-07-20 16:40:47 -0300

    feat(tempconv) - Adicionando a conversão de Celsius para Kelvin

--- tempconv/tempconv.go
+++ tempconv/tempconv.go
@@ -1,10 +1,11 @@
-// tempconv realiza conversões de Celsius e Fahrenheit
+// tempconv realiza conversões de Celsius, Fahrenheit e Kelvin
 package tempconv
 
 import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	// AbsoluteZeroC representa a temperatura zero absoluto em Celsius
@@ -23,8 +24,14 @@ func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
 // String imprime uma temperatura 'n' em Fahrenheit no formato n°F
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
 
+// String imprime uma temperatura 'n' em Kelvin no formato nK
+func (k Kelvin) String() string { return fmt.Sprintf("%gK", f) }
+
 // CToF converte uma temperatura em Celsius para Fahrenheit
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
 // FToC converte uma temperatura em Fahrenheit para Celsius
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// CToK converte uma temperatura em Celsius para Kelvin
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273) }
