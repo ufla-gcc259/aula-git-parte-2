4808c35cf76e1d7159fb5c375f1302bced3d8449
Mateus Tudeia <mateustudeia@ioasys.com.br>
2022-07-31 23:08:47 -0300

    Criados testes para tradução c#

---
+++ outras_linguagens/CSharp/tempconv/formatter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tempconv{
+    public class TemperatureFormatter{
+        public static String FormatCelsius(double cels){
+            return cels.ToString() + " ºC";
+        }
+
+        public static String FormatFahrenheit(double fahr){
+            return fahr.ToString() + " ºF";
+        }
+    }
+}
