201e1f58ccd5b63b1f5ebd7865630c1e38017d0c
arthur.franco <arthur.franco@estudante.ufla.br>
2022-12-08 15:03:45 -0300

    Codigo Exemplo de uso da biblioteca

--- exemple.go
+++ exemple.go
@@ -1,35 +1,9 @@
-*tempconv*
-=====
-Biblioteca escrita em Go para conversões simples de temperatura. Extraída do livro **A Linguagem de Programação Go**, de Alan A. A. Donovan e Brian Kernighan. 
-
-Como usar?
-----
-Faça o download da biblioteca:
-
-`go get https://github.com/ufla-gcc259/aula-git-parte-2@v1.0.1`
-
-Pronto, agora é só usar:
-```go
 package main
-
-import (
-	"fmt"
-
-	"github.com/ufla-gcc259/aula-git-parte-2/tempconv"
-)
-
-func main() {
-	fmt.Printf("Que frio! %v\n", tempconv.AbsoluteZeroC) // Que frio! -273.15°C
-	fmt.Printf("Fervendo! %v\n", tempconv.CToF(tempconv.BoilingC)) // Fervendo! 212°F
-}
-```
-Exemplo de Uso:
-----
-
-```go
-
-package main
-
+/* 
+* Este códio é um exemplo de uso da biblioteca tempconv.
+* 
+*
+*/
 import (
 	"fmt"
 	"os"
@@ -70,21 +44,3 @@ func main() {
 func uso(){
 	fmt.Println("Para executar corretamente o exemplo, passe apenas um valor numérico como parametro:\ngo run ./app <valor numérico>")
 }
-
-```
-Outras linguagens?
-----
-Versões da biblioteca *tempconv* para outras linguagens:
-
-> *Todo*
-
-
-Licença
------
-
-> *Todo*
-
-
-Como contribuir?
-----
-Escolha uma *issue* dentre as [disponíveis](https://github.com/ufla-gcc259/aula-git-parte-2/issues), avise à comunidade que você está trabalhando nela e envie um *Pull Request*, quando terminar.
