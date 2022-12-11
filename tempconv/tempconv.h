350f95c7db2f7b6f6cfaea5c3ca4f20ec936c989
luizdopc <luizotaviosoares@outlook.com>
2022-12-11 15:40:52 -0300

    Traduzindo para C++

---
+++ tempconv/tempconv.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+const float AbsoluteZeroC = -273.15;
+const float FreezingC = 0;
+const float BoilingC = 100;
+
+float CToF(float c){
+  return c*9/5+32;
+}
+
+float FToC(float f){
+  return (f-32)*5/9;
+}
