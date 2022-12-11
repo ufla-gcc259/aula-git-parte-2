72b26e3cac9f44145a73c67b17daaabb37c1a329
MattAugusto00 <58348794+MattAugusto00@users.noreply.github.com>
2022-12-11 00:58:41 -0300

    Update tempconv.go

--- tempconv/tempconv.go
+++ tempconv/tempconv.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	// AbsoluteZeroC representa a temperatura zero absoluto em Celsius
@@ -22,8 +23,23 @@ func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
 // String imprime uma temperatura 'n' em Fahrenheit no formato n°F
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
 
+// String imprime uma temperatura 'n' em Kelvin no formato n°K
+func (k Fahrenheit) String() string { return fmt.Sprintf("%g°K", k) }
+
 // CToF converte uma temperatura em Celsius para Fahrenheit
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func CToF(c Celsius) Fahrenheit { return Fahrenheit((c * 9 / 5) + 32) }
+
+// CToF converte uma temperatura em Celsius para Kelvin
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
 
 // FToC converte uma temperatura em Fahrenheit para Celsius
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// FToC converte uma temperatura em Fahrenheit para Kelvin
+func FToK(f Fahrenheit) Kelvin { return Kelvin(((f - 32) * 5 / 9) + 273.15) }
+
+// KToC converte uma temperatura em Kelvin para Celsius
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+// KToF converte uma temperatura em Kelvin para Fahrenheit
+func KToF(k Kelvin) Fahrenheit { return Fahrenheit(((k - 273.15) * 9 / 5) + 32) }
