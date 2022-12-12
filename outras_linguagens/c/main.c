d8dd6104448220391656814c8ba6cac04542faee
Vinicius Caputo <74276895+vinicius-caputo@users.noreply.github.com>
2022-12-11 23:26:40 -0300

    Delete main.c

--- outras_linguagens/c/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "tempconv.h"
-
-int main(){
-
-    printf("Que frio!: %.2f\n", AbsoluteZeroC);
-    printf("Fervendo!: %s\n", formatFahrenheit(CToF((float)BoilingC)));
-}
