ec8340ae6c9e8e75a5772146fcc051532629abc2
Paulo Júnior <pauloa.junior@ufla.br>
2023-10-02 21:36:04 -0300

    Adicionando comentários

--- tempconv/tempconv.go
+++ tempconv/tempconv.go
@@ -17,6 +17,8 @@ func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
 
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
 
+// CToF converte uma temperatura em Celsius para Fahrenheit
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
+// FToC converte uma temperatura em Fahrenheit para Celsius
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
