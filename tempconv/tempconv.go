dac3b2a4672833875a2fef52631b7f1a85625c3c
Vidaflor111 <vidaflor.costa@estudante.ufla.br>
2023-10-07 16:30:35 -0300

    Considerar escala Kelvin

--- tempconv/tempconv.go
+++ tempconv/tempconv.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	// AbsoluteZeroC representa a temperatura zero absoluto em Celsius
@@ -14,6 +15,9 @@ const (
 
 	// BoilingC representa a temperatura de ebulição da água em Celsius
 	BoilingC Celsius = 100
+
+	// AbsoluteZeroK representa a temperatura zero absoluto em Kelvin
+	AbsoluteZeroK Kelvin = 0
 )
 
 // String imprime uma temperatura 'n' em Celsius no formato n°C
@@ -22,8 +26,17 @@ func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
 // String imprime uma temperatura 'n' em Fahrenheit no formato n°F
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
 
+// String imprime uma temperatura 'n' em Kelvin no formato nK
+func (k Kelvin) String() string { return fmt.Sprintf("%gK", k) }
+
 // CToF converte uma temperatura em Celsius para Fahrenheit
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
 // FToC converte uma temperatura em Fahrenheit para Celsius
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// CToK converte uma temperatura em Celsius para Kelvin
+func CToK(c Celsius) Kelvin { return Kelvin(c - AbsoluteZeroC) }
+
+// KToC converte uma temperatura em Kelvin para Celsius
+func KToC(k Kelvin) Celsius { return Celsius(k) + AbsoluteZeroC }
