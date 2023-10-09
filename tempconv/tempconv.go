11af155923985e8e24f0c576257bf8c56dbafbb2
luizeduardo7 <luisedu11201184@gmail.com>
2023-10-09 12:54:27 -0300

    Funcoes de conversao para Kelvin

--- tempconv/tempconv.go
+++ tempconv/tempconv.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	// AbsoluteZeroC representa a temperatura zero absoluto em Celsius
@@ -26,4 +27,16 @@ func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
 // FToC converte uma temperatura em Fahrenheit para Celsius
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5/9) }
+
+// FToK converte uma temperatura em Fahrenheit para Kelvin
+func FToK(f Fahrenheit) Kelvin { return Kelvin((f - 32) * 5/9 + 273.15) }
+
+// CToK converte uma temperatura em Celsius para Kelvin
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+
+// KToF converte uma temperatura em Kelvin para Fahrenheit
+func CToF(k Kelvin) Fahrenheit { return Fahrenheit((k - 273.15) * 9/5 + 32) }
+
+// KToC converte uma temperatura em Kelvin para Celsius
+func KToC(k Kelvin) Celsius { return Celsius(c - 273.15) }
